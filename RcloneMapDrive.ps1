e4cd617900b7eae289bac5910b53edae976a9e52
dinger1986 <daniel.lamb@flonix.co.uk>
2022-09-19 11:05:51 +0100

    Update RcloneMapDrive.ps1

--- RcloneMapDrive.ps1
+++ RcloneMapDrive.ps1
@@ -35,7 +35,7 @@ new-item "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Progra
 New-Item "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName.bat"
 New-Item "C:\Users\$username\AppData\Roaming\techahold\$RemoteName $username.bat"
 
-Set-Content "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName.bat" "@echo off `necho Setup your cloud connection now `ncd $rclonedir`nrclone config"
+Set-Content "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName.bat" "@echo off `necho Setup your cloud connection now `ncd $rclonedir`nrclone config`npowershell.exe"
 
 
 Set-Content "C:\Users\$username\AppData\Roaming\techahold\$RemoteName $username.bat" "$rclonedir\rclone mount ${sharepoint}:/ P: --volname $MapDriveName --vfs-cache-mode off --no-console --log-file %LOCALAPPDATA%\rclone\logs\driveP.txt"
