7cbea5732220b213424a36d9ada17a483730e97f
dinger1986 <daniel.lamb@flonix.co.uk>
2023-02-09 22:40:56 +0000

    Update RcloneMapDrive.ps1

--- RcloneMapDrive.ps1
+++ RcloneMapDrive.ps1
@@ -2,7 +2,7 @@
 $username = ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
 
 $rclonedir="C:\Program Files\Rclone"
-$RemoteName="Techahold Map Drive"
+$RemoteName="Techahold"
 $MapDriveName="\\server\shared"
 #$tag = (Invoke-WebRequest "https://api.github.com/repos/rclone/rclone/releases/latest" | ConvertFrom-Json)[0].tag_name
 
@@ -23,20 +23,17 @@ new-item "C:\Users\$username\AppData\Local\rclone\logs" -itemtype directory
 new-item "C:\Users\$username\AppData\Roaming\techahold\" -itemtype directory
 new-item "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\$RemoteName" -itemtype directory
 
-New-Item "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName.bat"
-New-Item "C:\Users\$username\AppData\Roaming\techahold\$RemoteName $username.bat"
-New-Item "C:\Users\$username\AppData\Roaming\techahold\updateconfig.ps1"
+New-Item "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName Mapped Drive.bat"
+New-Item "C:\Users\$username\AppData\Roaming\techahold\$RemoteName Mapped Drive for $username.bat"
 
-Set-Content "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName.bat" "@echo off `necho Setup your cloud connection now `ncd $rclonedir`nrclone config`npowershell.exe C:\Users\$username\AppData\Roaming\techahold\updateconfig.ps1"
-Set-Content "C:\Users\$username\AppData\Roaming\techahold\$RemoteName $username.bat" "@echo off `ncd $rclonedir`nrclone mount remotename:/ P: --volname $MapDriveName --vfs-cache-mode full --ignore-checksum --ignore-size --no-console --log-file %LOCALAPPDATA%\rclone\logs\driveP.txt"
-Set-Content "C:\Users\$username\AppData\Roaming\techahold\updateconfig.ps1" "`$RemoteInput= Get-Content C:\Users\$username\AppData\Roaming\rclone\rclone.conf -First 1`n`$RemoteInput= `$RemoteInput -replace '[][]'`n((Get-Content -path 'C:\Users\$username\AppData\Roaming\techahold\$RemoteName $username.bat' -Raw) -replace 'remotename',`$RemoteInput) | Set-Content -Path 'C:\Users\$username\AppData\Roaming\techahold\$RemoteName $username.bat'`nCopy-Item 'C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\$RemoteName\$RemoteName for $username.lnk' -Destination 'C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$RemoteName for $username.lnk'`ncd '$rclonedir'`nrclone mount `$RemoteInput:/ P: --volname $MapDriveName --vfs-cache-mode full --no-console --log-file %LOCALAPPDATA%\rclone\logs\driveP.txt"
-
+Set-Content "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName Mapped Drive.bat" "@echo off `necho Setup your cloud connection now use $RemoteName when asked `ncd ""$rclonedir""`nrclone.exe config`nrclone.exe mount $RemoteName:/ P: --volname $MapDriveName --vfs-cache-mode full --ignore-checksum --ignore-size --no-console --log-file %LOCALAPPDATA%\rclone\logs\driveP.txt"
+Set-Content "C:\Users\$username\AppData\Roaming\techahold\$RemoteName Mapped Drive for $username.bat" "@echo off `ncd ""$rclonedir""`nrclone.exe mount $RemoteName:/ P: --volname $MapDriveName --vfs-cache-mode full --ignore-checksum --ignore-size --no-console --log-file %LOCALAPPDATA%\rclone\logs\driveP.txt"
 
 $ShortcutPath = "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\$RemoteName\Setup $RemoteName.lnk"
 $IconLocation = "$rclonedir\rclone.exe"
 $Shell = New-Object -ComObject ("WScript.Shell")
 $Shortcut = $Shell.CreateShortcut($ShortcutPath)
-$Shortcut.TargetPath = "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName.bat"
+$Shortcut.TargetPath = "C:\Users\$username\AppData\Roaming\techahold\Setup $RemoteName Mapped Drive.bat"
 $Shortcut.IconLocation = "$IconLocation, $IconArrayIndex"
 $Shortcut.Save()
 
@@ -44,9 +41,11 @@ $ShortcutPath = "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu
 $IconLocation = "$rclonedir\rclone.exe"
 $Shell = New-Object -ComObject ("WScript.Shell")
 $Shortcut = $Shell.CreateShortcut($ShortcutPath)
-$Shortcut.TargetPath = "C:\Users\$username\AppData\Roaming\techahold\$RemoteName $username.bat"
+$Shortcut.TargetPath = "C:\Users\$username\AppData\Roaming\techahold\$RemoteName Mapped Drive for $username.bat"
 $Shortcut.IconLocation = "$IconLocation, $IconArrayIndex"
 $Shortcut.Save()
+
+Copy-Item "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\$RemoteName\$RemoteName for $username.lnk" -Destination "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 
 # Open Config Folder
 explorer.exe /e,"C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\$RemoteName"
